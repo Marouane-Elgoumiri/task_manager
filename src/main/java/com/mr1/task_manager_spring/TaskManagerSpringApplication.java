e604afe33e74614ddb1a3c5a34e5f398af07c405
Marouane-Elgoumiri <mar.elgoumiri@hotmail.com>
2024-03-24 15:08:38 +0000

    first push

---
+++ src/main/java/com/mr1/task_manager_spring/TaskManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mr1.task_manager_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mr1/task_manager_spring/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr1.task_manager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
